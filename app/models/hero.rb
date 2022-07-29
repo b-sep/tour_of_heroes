c26371adaaefdb8d649f259babf667d4fb76aa79
Júnior <b.sep@live.com>
2022-07-29 20:19:26 -0300

    Adiciona testes model hero

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,6 +1,7 @@
 class Hero < ApplicationRecord
-  validates :name, :token, presence: true
   validates :name, uniqueness: { scope: :token, case_sensitive: false }
+  validates :name, :token, presence: true
+  validates :token, length: { minimum: 10 }
   
   scope :sorted_by_name, -> { order :name }
   scope :search_by_token, -> (token) { where(token: token) }
